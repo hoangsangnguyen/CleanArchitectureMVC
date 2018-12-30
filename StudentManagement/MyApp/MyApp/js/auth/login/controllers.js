aceb79a222721472e7da50bdb493807e7ffb1686
hoangsangnguyen <nguyenhoangsang6014@gmail.com>
2018-12-30 23:54:04 +0700

    init front end

---
+++ StudentManagement/MyApp/MyApp/js/auth/login/controllers.js
@@ -0,0 +1,36 @@
+﻿/* global angular */
+(function () {
+    "use strict";
+
+    var app = angular.module('login.controllers', []);
+    var url = 'http://localhost/Backend';
+
+    app.controller('loginCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+            $scope.login = function () {
+                $http.post(url + '/auth', { 'Username': $scope.userName, 'Password': $scope.password })
+                    .success(function (response) {
+                        console.log('Response ', response);
+                    });
+            }
+
+            var data = {
+                'Username' : $scope.userName,
+                'Password' : $scope.password
+            };
+            //$scope.$watch('name', function () {
+            //    if ($scope.name) {
+            //        $http.get('/hello/' + $scope.name)
+            //            .success(function (response) {
+            //                $scope.helloResult = response.Result;
+            //            });
+            //    }
+            //});
+
+            //$scope.testFunction = function () {
+            //    return true;
+            //};
+        }
+    ]);
+})();
+
